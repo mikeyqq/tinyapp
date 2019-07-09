a0c23f277a4e4b963b9d725b109f6377155bfb47
Anchen <acmhuang@gmail.com>
2019-07-08 20:31:32 -0400

    added redirect of short link

--- express_server.js
+++ express_server.js
@@ -36,6 +36,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -46,8 +50,10 @@ app.get('/hello', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  urlDatabase.generateRandomString = req.body[longURL];  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newsmallLink = generateRandomString();
+  urlDatabase[newsmallLink] = req.body['longURL'];  // Log the POST request body to the console  
+  res.redirect(`/urls/${newsmallLink}`)
+  console.log(urlDatabase);
 });
 
 app.listen(PORT, () => {
